f514e6a4caab1f81d785f97a699852d6687fbee2
Djibril SOW <djibril.sow9717@gmail.com>
2024-02-03 13:14:14 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import React, { useState, useEffect } from 'react';
+
+const App = () => {
+
+  const person = {
+    fullName: 'Djibril SOW',
+    bio: "Je suis un développeur web ASP.NET/JAVA | Angular/React/NodeJS. Je maîtrise plusieurs langages informatiques et je suis capable de travailler sur différentes plateformes et en équipe. Je suis également autonome, disponible et capable d'intervenir sur toutes les étapes d'un projet informatique et d'apprendre très rapidement de nouvelles technologies.",
+    imgSrc: 'https://media.licdn.com/dms/image/C4E03AQEEavGd452dBg/profile-displayphoto-shrink_400_400/0/1661770568868?e=1712188800&v=beta&t=0Ohtj5QudjkFWnTFNZ637Xnrv4p9zor3F8cQl5FAXjo',
+    profession: 'Développeur FullStack',
+  };
+
+  const [show, setShow] = useState(false);
+  const [timeValue, setTimeValue] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimeValue((prevTimeValue) => prevTimeValue + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const handleShow = () => {
+    setShow((prevShow) => !prevShow);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <button onClick={handleShow}>Afficher profile</button>
+      {show && (
+        <div>
+          <h2>{person.fullName}</h2>
+          <p>{person.bio}</p>
+          <img src={person.imgSrc} alt="Person" />
+          <p>Profession: {person.profession}</p>
+        </div>
+      )}
+      <p>Interval de temps: {timeValue} seconds</p>
     </div>
   );
-}
+};
 
 export default App;
+
